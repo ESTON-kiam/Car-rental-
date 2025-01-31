b8d1978dd95cbc0d314d75f0376755afb8f0fcc3
ESTON-kiam <engestonbrandonkiama@gmail.com>
2025-01-31 10:01:08 +0300

    update database

--- Database/adata.sql
+++ Database/adata.sql
@@ -1,23 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 16, 2025 at 08:51 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
 -- Database: `car_rental_management`
 --
 CREATE DATABASE IF NOT EXISTS `car_rental_management` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
